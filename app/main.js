cc94d8956afd7177757442c033bf78f843e9f749
Thomas Scott <thomas.scott@squidsolutions.com>
2016-03-14 09:46:48 +0100

    provide a sample data table visulisation in D3 and implement bookmarks

--- app/main.js
+++ app/main.js
@@ -17,22 +17,71 @@ $( document ).ready(function() {
      */
     new API.controller.FiltersController();
     var analysis = (new API.controller.AnalysisController()).model;
-    
+
     /*
-     * Declare the views 
+     * Declare the views
      */
     new API.view.LoginView();
     new API.view.StatusView();
-    new API.view.DataTableView ({
-        el : '#data',
-        model : analysis
-    });
     new API.view.CategoricalView({
         el : '#filters',
         filterSelected : '#selected'
     });
     new API.view.DateSelectionWidget({
         el : '#date'
+    });
+    new API.view.BookmarkWidget({
+        el : '#bookmark'
+    });
+
+    analysis.on("change", function() {
+        if (this.get("results")) {
+            /*
+                Sample D3 Table to display the data
+             */
+
+            // remove any existing tables created
+            d3.select('#data table').remove();
+
+            // specify the rendering div
+            var container = d3.select('#data');
+
+            // create table & append table headers
+            container.append('table')
+                     .append('thead')
+                     .append('tr');
+
+            // store our created table
+            var table = container.select('table');
+
+                // insert table header data
+                table.select("thead tr")
+                     .selectAll("th")
+                     .data(this.get("results").cols)
+                     .enter()
+                     .append("th")
+                     .text(function(d) {
+                        return d.name;
+                     });
+
+                // insert table body
+                table.append('tbody');
+
+                // insert table body data
+                table.select("tbody")
+                    .selectAll("tr")
+                    .data(this.get("results").rows)
+                    .enter()
+                    .append("tr").selectAll("td")
+                    .data(function(d) {
+                        return d.v;
+                    })
+                    .enter()
+                    .append("td")
+                    .text(function(d) {
+                        return d;
+                    });
+        }
     });
     
     /*
